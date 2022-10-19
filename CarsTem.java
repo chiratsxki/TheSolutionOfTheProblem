11623a9293ee1fef4cb8f2b6c3d65a57916d4103
Maksim <107635322+chiratsxki@users.noreply.github.com>
2022-10-19 19:48:18 +0500

    Add files via upload

---
+++ CarsTem.java
@@ -0,0 +1,29 @@
+public class CarsTem {
+    public static void main(String[] args) {
+        int[] stations = {0, 200, 375, 550, 750, 950};
+        System.out.println(minStops(stations, 400));
+    }
+    // returns -1 if it's impossible to get from A to B
+    public static int minStops(int[] stations, int capacity) {
+        int result = 0; //optimal number of stops
+        int currentStop  = 0;
+
+
+        while (currentStop < stations.length - 1){
+            int nextStop = currentStop;
+
+            while(nextStop < stations.length - 1  && stations[nextStop + 1] - stations[currentStop] <= capacity  ){
+                nextStop++;
+            }
+            if(currentStop == nextStop){
+                return -1;
+            }
+            if(nextStop < stations.length - 1 ){
+                result ++;
+            }
+
+            currentStop = nextStop;
+        }
+        return  result;
+    }
+}
